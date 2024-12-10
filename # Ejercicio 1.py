3329b2ac5b2a3f3173caf97779de7878f3ff1774
mariasg19 <m.sanz@accenture.com>
2024-12-10 16:38:28 +0100

    Add files via upload

---
+++ # Ejercicio 1.py
@@ -0,0 +1,42 @@
+# Ejercicio 1------------------------------------------------------------
+"""
+Sumar dos numeros y mostrar su resultado
+"""
+
+num1 = 5
+num2 = 4
+
+resultado = num1 + num2
+
+print(resultado)
+
+# Ejercicio 2------------------------------------------------------------
+"""Calcula el area de un circulo
+con un radio dado"""
+
+#Aqui ya empezamos a usar las librerias 
+#porque necesitamos la constante de pi
+
+import math
+
+radio = 3
+area = math.pi * radio **2
+print("El area es ", area)
+
+
+# Ejercicio 3------------------------------------------------------------
+""" Concatena dos cadenas de texto"""
+
+cadena1 = "Hola"
+cadena2 = "caracola"
+
+concatenacion = cadena1 + " " + cadena2
+
+print("Mi cadena es ",concatenacion)
+
+# Ejercicio 4------------------------------------------------------------
+"Crear una lista con diferentes elementos e imprimirla"
+
+lista1 = [4, True, "Hola"]
+
+print ("elementos: ", lista1)
