ad77e0d0b318fa22ff8050dba6cd99903b900cef
mariasg19 <m.sanz@accenture.com>
2024-12-11 16:57:01 +0100

    Add files via upload

---
+++ Mas ejercicios de por la tarde.py
@@ -0,0 +1,62 @@
+#Mas ejercicios de por la tarde
+
+# Ejercico 14
+#Reemplaza un caracter en una cadena
+
+cadena = "python"
+nueva_cadena = cadena.replace("y", "x")
+
+print(nueva_cadena)
+
+#Ejercicio 15
+#Pasa una cadena de mayusculas a minusculas
+cadena_mayusculas = cadena.upper()
+print(cadena_mayusculas)
+
+#Ejercicio 16
+#Ordena una lista de numeros de menos a mayor
+
+lista = [3,5,1,8,5]
+lista.sort()
+print(lista)
+
+#Ejercicio 17
+#Calcula 2 elevado a 4 sin usar **
+
+resultado = pow(2,4)
+print(resultado)
+
+#Ejercicio 17
+#Extrae una subcadena de una cadena dada
+cadena2 = "holi"
+subcadena = cadena[0:2]
+print(subcadena)
+
+#Ejercicio 18
+#Convierte un numero decimal a un numero entero
+num1 = 4.5
+num2 = int(num1)
+
+print(num2)
+
+#Ejercicio 19
+#Cuenta el n de veces que aparece un caracter 
+#en una cadena
+
+cadena4 = "hohohoho"
+n_veces = cadena4.count("o")
+print(n_veces)
+
+#Ejercicio 20
+#Encuentra y muestra el ultimo caracter de una cadena
+
+cadena5 = "caballos y vacas"
+ulti_caract = cadena5[-1]
+print(ulti_caract)
+
+#Ejercicio 21
+# multiplica una cadena por un numero entero
+
+cadena6 = "holi"
+multiplicado = cadena6 * 4
+print(multiplicado)
