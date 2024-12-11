02dc7e350cae7d6294fc77e3c62018b376e848f5
mariasg19 <m.sanz@accenture.com>
2024-12-11 11:00:12 +0100

    Add files via upload

---
+++ Ejercicio 10.py
@@ -0,0 +1,21 @@
+# Ejercicio 10 USANDO FUNCION TYPE
+"""Imprime una cadena con distintos datos
+e indica el tipo de datos que son"""
+cadena = (5,4,True,"Holi")
+print(cadena)
+#El type no se puede aplicar directamente sobre una cadena
+print(type("Ho"),type(4),type(True))
+
+#Ejercicio 11 OPERADORES ARITMETICOS
+num1 = 3
+num2 = 5
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+#EXPONENTE 3*3*3*3*3
+print(num1**num2)
+print(num1/num2)
+#DIVISION CON RDO SIN DECIMALES REDONDEANDO A LA BAJA
+print(num1//num2)
+print(num2%num1)
+
