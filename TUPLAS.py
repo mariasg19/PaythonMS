98abfc57f4c76973b08c7e33d558294f6e92f208
mariasg19 <m.sanz@accenture.com>
2024-12-11 12:57:42 +0100

    Add files via upload

--- TUPLAS.py
+++ TUPLAS.py
@@ -36,3 +36,20 @@ print(f"El maximo de mi tupla es {maximo}")
 
 minimo = min(tupla3)
 print(f"El minimo de mi tupla es {minimo}")
+
+# Calcula los cuadrados de la tupla con una función de compresión
+
+tuplaCuadrados = tuple(x**2 for x in tupla3)
+
+# Imprimir la nueva tupla con los cuadrados
+print(tuplaCuadrados)
+
+# Desempaqueta la tupla en tantas variables como elementos tenga
+
+tupla4 = 4,2,6,7,"3"
+a,b,c,d,e = tupla4
+print(f"a: {a}")
+print(f"b: {b}")
+print(f"c: {c}")
+print(f"d: {d}")
+print(f"e: {e}")
